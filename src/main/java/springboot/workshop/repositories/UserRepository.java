db4abc4fa3c939fbe6162f41d3ceadaeae2bb6ce
Marcos Macedo <ricardo.paiva.macedo@hotmail.com>
2023-08-30 01:16:47 -0300

    Camada de serviço, registro de componentes

--- src/main/java/springboot/workshop/repositories/UserRepository.java
+++ src/main/java/springboot/workshop/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package springboot.workshop.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import springboot.workshop.entites.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
---
+++ src/main/java/springboot/workshop/services/UserService.java
@@ -0,0 +1,28 @@
+package springboot.workshop.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import springboot.workshop.entites.User;
+import springboot.workshop.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	private UserRepository userRepository;
+	
+	public List<User> findAll() {
+		
+		return userRepository.findAll();
+	}
+	
+	public User findById(Integer id) {
+		
+		Optional<User> opt = userRepository.findById(id);
+		return opt.get();
+	}
+	
+
+}
