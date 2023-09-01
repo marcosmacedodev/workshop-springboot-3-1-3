7760388db7ec6c5987d3fdab3ba0a9129888afd6
Marcos Macedo <ricardo.paiva.macedo@hotmail.com>
2023-09-01 01:20:44 -0300

    Tratamento de exceção - delete

--- src/main/java/springboot/workshop/controllers/exceptions/ResourceExceptionHandler.java
+++ src/main/java/springboot/workshop/controllers/exceptions/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package springboot.workshop.controllers.exceptions;
 
 import java.time.Instant;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,5 +19,12 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(Instant.now(), HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND.getReasonPhrase(), e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> notFound(DataIntegrityViolationException e, HttpServletRequest request){
+		StandardError err = new StandardError(Instant.now(), HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
 
 }
